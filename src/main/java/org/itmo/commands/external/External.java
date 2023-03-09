82cfc3f879f533f027b1522c6aa47ecab03b860b
Vasya <khitrik.rozanova.svetlana@gmail.com>
2023-03-09 10:31:08 +0300

    feat: add external

---
+++ src/main/java/org/itmo/commands/external/External.java
@@ -0,0 +1,55 @@
+package org.itmo.commands.external;
+
+import org.itmo.commands.Command;
+import org.itmo.utils.CommandInfo;
+import org.itmo.utils.CommandResultSaver;
+
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.util.List;
+
+public class External implements Command {
+    
+    private String name;
+    
+    private final List<String> params;
+    
+    boolean isWindows;
+    
+    public External(CommandInfo commandInfo) {
+        isWindows = System.getProperty("os.name").toLowerCase().startsWith("windows");
+        name = commandInfo.getCommandName();
+        params = commandInfo.getParams();
+    }
+    
+    /**
+     *
+     * Run users command
+     */
+    @Override
+    public void execute() throws Exception {
+        ProcessBuilder builder = new ProcessBuilder();
+        if (isWindows) {
+            builder.command("cmd.exe", "/c", name + params.toString());
+        } else {
+            builder.command("sh", "-c", name + params.toString());
+        }
+        builder.directory(new File(System.getProperty("user.home")));
+//        builder.redirectInput()   //?
+        Process process = builder.start();
+        process.waitFor();
+        try (var reader = new BufferedReader(new InputStreamReader(process.getInputStream())))
+        {
+            String line;
+            while ((line = reader.readLine()) != null)
+            {
+                CommandResultSaver.saveCommandResult(line, true);
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+    
+}

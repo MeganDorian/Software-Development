edd6430f9c47d4ba3268847b21196d03660e1bdc
megan <mishakollins68@gmail.com>
2023-03-09 05:31:18 +0300

    feat(test): added tests for Exit command

---
+++ src/test/java/org/itmo/commands/exit/ExitTests.java
@@ -0,0 +1,29 @@
+package org.itmo.commands.exit;
+
+import org.itmo.utils.CommandResultSaver;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+class ExitTests {
+    
+    @BeforeEach
+    public void setUp() throws IOException {
+        CommandResultSaver.createCommandResultFile();
+    }
+    
+    @Test
+    public void shouldExitAndDeleteFile() {
+        File file = CommandResultSaver.getResult().toFile();
+        
+        Exit exit = new Exit();
+        assertDoesNotThrow(exit::execute);
+        
+        assertFalse(file.exists());
+    }
+}

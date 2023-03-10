392a4da3774012ee6dcd141ee3975fb3641dd4b2
Vasya <khitrik.rozanova.svetlana@gmail.com>
2023-03-10 11:24:25 +0300

    fix: check flags

--- src/main/java/org/itmo/modules/Checker.java
+++ src/main/java/org/itmo/modules/Checker.java
@@ -29,7 +29,7 @@ public class Checker {
                 switch (com.getCommandName()) {
                     case "cat": {
                         for (int i = 0; i < com.getFlags().size(); i++) {
-                            if (!CatFlags.isBelongs(com.getParams().get(i))) {
+                            if (!CatFlags.isBelongs(com.getFlags().get(i))) {
                                 throw new FlagNotFoundException("cat: unrecognized option '"
                                         + com.getParams().get(i)
                                         + "'\nTry 'cat -h' for more information.");
@@ -38,7 +38,7 @@ public class Checker {
                     }
                     case "pwd": {
                         for (int i = 0; i < com.getFlags().size(); i++) {
-                            if (!PwdFlags.isBelongs(com.getParams().get(i))) {
+                            if (!PwdFlags.isBelongs(com.getFlags().get(i))) {
                                 throw new FlagNotFoundException("pwd: unrecognized option '"
                                         + com.getParams().get(i)
                                         + "'\nTry 'pwd -h' for more information.");
@@ -47,7 +47,7 @@ public class Checker {
                     }
                     case "wc": {
                         for (int i = 0; i < com.getFlags().size(); i++) {
-                            if (!WcFlags.isBelongs(com.getParams().get(i))) {
+                            if (!WcFlags.isBelongs(com.getFlags().get(i))) {
                                 throw new FlagNotFoundException("wc: unrecognized option '"
                                         + com.getParams().get(i)
                                         + "'\nTry 'wc -h' for more information.");
